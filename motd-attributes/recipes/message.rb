f19a29550fb0aad0b0f5d1b2fc499fe7b08d12d9
Nick White <nickwhite44@gmail.com>
2015-06-12 16:44:41 -0600

    Initial Commit

---
+++ motd-attributes/recipes/message.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: motd-attributes
+# Recipe:: message
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+node.default['motd-attributes']['company'] = 'the best company in the universe - Cloudera!'
